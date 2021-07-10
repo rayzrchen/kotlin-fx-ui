185806d88dff7147d4e93e25827513391af9e248
rayzrchen <rayzrchen@gmail.com>
2021-07-10 10:50:02 +0800

    init

---
+++ src/test/kotlin/com/example/kotlinjavafx/KotlinJavafxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinjavafx
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinJavafxApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
